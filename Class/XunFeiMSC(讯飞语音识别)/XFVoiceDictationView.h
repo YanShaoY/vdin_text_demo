c22692cca3710585ef1231f9440aae4d9c6dd811
YanSY <776046070@qq.com>
2018-05-14 13:43:35 +0800

    添加的新文件

---
+++ Class/XunFeiMSC(讯飞语音识别)/XFVoiceDictationView.h
@@ -0,0 +1,13 @@
+//
+//  XFVoiceDictationView.h
+//  Demo
+//
+//  Created by YanSY on 2018/5/13.
+//  Copyright © 2018年 YanSY. All rights reserved.
+//
+
+#import "BaseView.h"
+
+@interface XFVoiceDictationView : BaseView
+
+@end
--- Service/005 语音识别服务/GASpeechTextMSCService.h
+++ Service/005 语音识别服务/GASpeechTextMSCService.h
@@ -7,8 +7,30 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "IFlyMSC/IFlyMSC.h"
 
-@interface GASpeechTextMSCService : NSObject
+// 类名声明
+@class IFlyDataUploader;
+@class IFlyPcmRecorder;
+@class IFlySpeechRecognizer;
+
+/**
+ 语音文字识别服务
+ */
+@interface GASpeechTextMSCService : NSObject<IFlySpeechRecognizerDelegate,IFlyRecognizerViewDelegate,IFlyPcmRecorderDelegate>
+
+@property (nonatomic, strong) NSString *pcmFilePath;                      //音频文件路径
+@property (nonatomic, strong) IFlySpeechRecognizer *iFlySpeechRecognizer; // 不带界面的识别对象
+@property (nonatomic, strong) IFlyRecognizerView *iflyRecognizerView;     // 带界面的识别对象
+@property (nonatomic, strong) IFlyDataUploader *uploader;                 // 数据上传对象
+
+@property (nonatomic, strong) NSString * result;           // 结果
+@property (nonatomic, assign) BOOL isCanceled;             // 是否取消
+
+@property (nonatomic,strong) IFlyPcmRecorder *pcmRecorder; // 录音器，用于音频流识别的数据传入
+@property (nonatomic,assign) BOOL isStreamRec;             // 是否是音频流识别
+@property (nonatomic,assign) BOOL isBeginOfSpeech;         // 是否返回BeginOfSpeech回调
+
 
 /**
  初始化语音识别服务
